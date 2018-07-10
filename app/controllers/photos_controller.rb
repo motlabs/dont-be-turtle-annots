c1d4f0348a3d58809076363dd52f8662b13aeb0a
Taekmin Kim <tantara.tm@gmail.com>
2018-07-11 05:23:28 +0900

    Add complex annotation tools

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,12 @@
 class PhotosController < ApplicationController
   def update
+    p = Photo.find(params[:id])
+    p.update(params[:photo])
+
+    done = (p.head_x > 0 || p.head_y > 0) && 
+    (p.neck_x > 0 || p.neck_y > 0) &&
+    (p.rshoulder_x > 0 || p.rshoulder_y > 0) &&
+    (p.lshoulder_x > 0 || p.lshoulder_y > 0)
+    p.update(done: done)
   end
 end
